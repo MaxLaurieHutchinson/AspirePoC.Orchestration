323c09e04a64ca7482563505c2ee3408b4c32ab6
Max Hutchinson <max.hutchinson258@gmail.com>
2024-06-18 17:46:09 +0100

    Multi Repo Commit test

--- Core/AppHost/Program.cs
+++ Core/AppHost/Program.cs
@@ -1,3 +1,18 @@
+using HotChocolate.Fusion.Aspire;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.Build().Run();
+var ActorService = builder.AddProject<Projects.AspirePoC_ActorService>("aspirepoc-actorservice");
+
+var BookService = builder.AddProject<Projects.AspirePoC_BookService>("aspirepoc-bookservice");
+
+builder.AddFusionGateway<Projects.AspirePoC_GatewayService>("aspirepoc-gatewayservice")
+        .WithOptions(new FusionCompositionOptions
+        {
+            EnableGlobalObjectIdentification = true
+        })
+        .WithSubgraph(ActorService)
+        .WithSubgraph(BookService);
+
+
+builder.Build().Compose().Run();
